ddc9380ac3df03e853f898a5f224b621ea65ad0f
michael bychkowski <mbychkowski@gmail.com>
2018-04-23 20:04:57 -0400

    add default navbar component

--- models/podcast.js
+++ models/podcast.js
@@ -1,2 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const podcastSchema = new Schema({
+  show: {
+    type: String,
+    required: true
+  },
+  host: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String
+  },
+  synopsis: {
+    type: String
+  },
+  lastRealease: {
+    type: Date
+  }
+});
+
+const Podcast = mongoose.model('Podcast', podcastSchema);
+
+module.exports = Podcast;
