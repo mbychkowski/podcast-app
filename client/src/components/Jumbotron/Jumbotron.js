57604001c97571010858379a61d12f7563f53ce8
mjsimon91 <mjsimon91@gmail.com>
2018-04-24 20:32:57 -0400

    Search component is in the JumboTron component and handles a user typing (HandleInputChange)

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Jumbotron.css";
+import Searchbar from "../Searchbar"
 
-const Jumbotron = props => {
-    return(
-        <div className="jumbotron w-full">
+class Jumbotron extends Component {
 
-        </div>
-    )
+// Handle the input change
+handleInputChange = event => {
+  const name = event.target.name
+  const value = event.target.value
+
+  this.setState({
+    [name]: value
+  })
 }
 
+// Handle the user searching for a podcast
+handleUserSearch = event => {
+  event.preventDefault();
+  //Search the iTunes API
+
+
+}
+
+
+// Display the content
+  render(){
+
+    return(
+        <div className="jumbotron w-full">
+          <Searchbar
+            handleInputChange = {this.handleInputChange}
+           />
+        </div>
+    )
+  }
+}
 //Export the Jumbotron
 export default Jumbotron;
