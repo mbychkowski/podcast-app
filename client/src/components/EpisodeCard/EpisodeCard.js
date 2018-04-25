36801ff7e1fc364dfc57def02174f9eeeb7beb26
michael bychkowski <mbychkowski@gmail.com>
2018-04-24 20:39:46 -0400

    initialize episode card

---
+++ client/src/components/EpisodeCard/EpisodeCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EpisodeCard = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ client/src/pages/Episodes/Episodes.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
+
+class Episodes extends Component {
+
+}
