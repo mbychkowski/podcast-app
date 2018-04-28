5bcc5ea167c8134bcebd48109d0f328c688baee3
michael bychkowski <mbychkowski@gmail.com>
2018-04-28 19:57:12 -0400

    okta setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,13 @@ import Navbar from "./components/Navbar"
 import Search from "./pages/Search"
 import Episodes from "./pages/Episodes"
 import "./App.css";
+import { Security, ImplicitCallback } from '@okta/okta-react';
+
+const config = {
+  issuer: 'https://dev-548725.oktapreview.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '{clientId}'
+}
 
 
 const App = () => (
@@ -11,8 +18,15 @@ const App = () => (
     <div>
       <Navbar />
       <Switch>
-        <Route exact path = "/" component = { Search } />
-        <Route exact path = "/podcast/:id" component = { Episodes } />
+        <Security issuer={config.issuer}
+          client_id={config.client_id}
+          redirect_uri={config.redirect_uri}
+        >
+          <Route exact path = "/" component = { Search } />
+          <Route exact path = "/podcast/:id" component = { Episodes } />
+
+          <Route exacr path = "/implicit/callback" component = { ImplicitCallback }/>
+        </Security>
       </Switch>
     </div>
   </Router>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,20 +1,51 @@
-import React from "react";
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { withAuth } from "@okta/okta-react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-// Create a component that will be the navbar
+export default withAuth(class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { authenticated: null };
+    this.checkAuthentication = this.checkAuthentication.bind(this);
+    this.checkAuthentication();
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
 
-const Navbar = props => {
-    return(
+  async checkAuthentication() {
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated });
+    }
+  }
+
+  componentDidUpdate() {
+    this.checkAuthentication();
+  }
+
+  async login() {
+    // Redirect to '/' after login
+    this.props.login('/');
+  }
+
+  async logout() {
+    // Redirect to '/' after logout
+    this.props.logout('/');
+  }
+
+  render() {
+    return (
       <nav className="flex items-center justify-between flex-wrap bg-teal p-6">
         <div className="flex items-center flex-no-shrink text-white mr-6">
           <Link to = {"/"}>
             <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
           </Link>
         </div>
-      </nav>
-    )
-}
-
-//Export the component
-export default Navbar
+        if (this.state.authenticated === null) return null;
+        return this.state.authenticated ?
+          <button onClick={this.logout}>Logout</button>
+          <button onClick={this.login}>Login</button>
+    </nav>
+  )}
+});
