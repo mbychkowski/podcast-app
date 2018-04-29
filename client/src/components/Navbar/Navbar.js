2a9e9b648d46fbde730bc7b7f743411adb2a1e3d
Andrew Zoka <mugendiz@gmail.com>
2018-04-28 23:16:43 -0400

    added button to login with Google pending logout route

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom'
+import GoogleLoginHelper from 'react-google-login';
+
+// import SignIn from './SignIn';
 import "./Navbar.css";
 
 // Create a component that will be the navbar
@@ -11,6 +14,11 @@ const Navbar = props => {
           <Link to = {"/"}>
             <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
           </Link>
+
+          {/* Sign-in with Google */}
+          <div className="g-signin2" data-onsuccess="onSignIn">
+              <GoogleLoginHelper />
+          </div>
         </div>
       </nav>
     )
