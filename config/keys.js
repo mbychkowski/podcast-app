ae5c7d3dd897ef88bb388063b7bad6ef9f003c15
Mugendi A. Zoka <32772345+mugendiz@users.noreply.github.com>
2018-04-30 01:22:42 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 
 module.exports = {
         google: {
-            clientID: '558027906323-30k4oqhfe0m38iupmhef8p1hid76v6i5.apps.googleusercontent.com',
-            clientSecret: 'OzdguWo6g2TaUfrhOe8lPGse'
+            clientID: 'keys.google.clientID',
+            clientSecret: 'keys.google.clientID'
         }
 };
