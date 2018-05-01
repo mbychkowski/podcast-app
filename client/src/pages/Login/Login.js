8b676b0e655b8fb49b768f592b139af4077380f0
michael bychkowski <mbychkowski@gmail.com>
2018-04-30 22:29:07 -0400

    auth updates

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import okta from "./keys";
 import OktaSignIn from "@okta/okta-signin-widget";
 import "@okta/okta-signin-widget/dist/css/okta-sign-in.min.css";
 import "@okta/okta-signin-widget/dist/css/okta-theme.css";
---
+++ client/src/pages/Login/keys.js
@@ -0,0 +1,8 @@
+console.log('keys loaded');
+
+const okta = {
+  client_id: process.env.CLIENT_ID,
+  base_url: process.env.BASE_URL
+}
+
+console.log(okta);
