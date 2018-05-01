cf726b224c41b7b33e731a825d3deb9d734c8a5c
michael bychkowski <mbychkowski@gmail.com>
2018-05-01 00:45:52 -0400

    css for navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withAuth } from '@okta/okta-react';
 
+import "./Navbar.css";
+
 export default withAuth(class Navbar extends Component {
   constructor(props) {
     super(props);
@@ -25,20 +27,23 @@ export default withAuth(class Navbar extends Component {
     if (this.state.authenticated === null) return null;
 
     const button = this.state.authenticated ?
-      <li><button onClick={this.props.auth.logout}>Logout</button></li> :
-      <li><button onClick={this.props.auth.login}>Login</button></li>;
+      <li><button className="mr-6" onClick={this.props.auth.logout}>Logout</button></li> :
+      <li><button className="mr-6" onClick={this.props.auth.login}>Login</button></li>;
 
     return (
-      <nav className="flex items-center justify-between flex-wrap bg-teal p-6">
-        <div className="flex items-center flex-no-shrink text-white mr-6">
-          <Link to = {"/"}>
-            <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-          </Link>
+      <nav className="flex items-center justify-between flex-wrap bg-blue-darkest p-6">
+        <div className="flex items-center flex-no-shrink mr-6">
+          <ul>
+            <li><Link to = {"/"}>
+              <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+              Orator
+            </Link></li>
+          </ul>
         </div>
         <div className="navbar-collapse collapse">
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/profile'>Profile</Link><br/></li>
+          <ul className="list-reset flex">
+            <li className="mr-6"><Link to='/'>Home</Link></li>
+            <li className="mr-6"><Link to='/profile'>Profile</Link><br/></li>
             {button}
           </ul>
         </div>
