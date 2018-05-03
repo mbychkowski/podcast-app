1ea79b8c91596ae431f6cfd7896d44a7eecb6c0b
michael bychkowski <mbychkowski@gmail.com>
2018-05-03 18:48:23 -0400

    cleanup for PR

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,9 @@
 import { withAuth } from '@okta/okta-react';
 import React, { Component } from 'react';
 import { Container, Icon, Image, Menu } from 'semantic-ui-react';
-import { checkAuthentication } from './helpers';
+import { checkAuthentication } from '../../helpers/helpers';
+
+import "./Navbar.css";
 
 export default withAuth(class Navbar extends Component {
   constructor(props) {
@@ -30,15 +32,14 @@ export default withAuth(class Navbar extends Component {
 
   render() {
     return (
+
       <div>
-        <Menu fixed="top" inverted>
+        <Menu className="bg-black" fixed="top" inverted>
           <Container>
             <Menu.Item as="a" header href="/">
-              <Image size="mini" src="/react.svg" />
-              &nbsp;
-              Okta-React Sample Project
+              <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+              <span className="font-semibold text-xl tracking-tight">Octar</span>
             </Menu.Item>
-            {this.state.authenticated === true && <Menu.Item id="messages-button" as="a" href="/messages"><Icon name="mail outline" />Messages</Menu.Item>}
             {this.state.authenticated === true && <Menu.Item id="profile-button" as="a" href="/profile">Profile</Menu.Item>}
             {this.state.authenticated === true && <Menu.Item id="logout-button" as="a" onClick={this.logout}>Logout</Menu.Item>}
             {this.state.authenticated === false && <Menu.Item as="a" onClick={this.login}>Login</Menu.Item>}
@@ -48,3 +49,31 @@ export default withAuth(class Navbar extends Component {
     );
   }
 });
+
+
+// <nav className="flex items-center justify-between flex-wrap bg-blue-darker p-6">
+//
+//   <div class="flex items-center flex-no-shrink text-white mr-6">
+//     <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+//     <span class="font-semibold text-xl tracking-tight">Orator</span>
+//   </div>
+//
+//   <div class="block lg:hidden">
+//     <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+//       <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+//     </button>
+//   </div>
+//
+//   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+//     <li className="mr-6">
+//       {this.state.authenticated === true && <a id="profile-button" as="a" href="/profile">Profile</a>}
+//     </li>
+//     <li className="mr-6">
+//       {this.state.authenticated === true && <a id="logout-button" as="a" onClick={this.logout}>Logout</a>}
+//     </li>
+//     <li className="mr-6">
+//       {this.state.authenticated === false && <a as="a" onClick={this.login}>Login</a>}
+//     </li>
+//   </div>
+//
+// </nav>
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,35 +1,3 @@
-// import React, { Component } from 'react';
-// import { Redirect } from 'react-router-dom';
-// import LoginForm from '../../components/LoginForm';
-// import { withAuth } from '@okta/okta-react';
-//
-// export default withAuth(class Login extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { authenticated: null };
-//     this.checkAuthentication = this.checkAuthentication.bind(this);
-//     this.checkAuthentication();
-//   }
-//
-//   async checkAuthentication() {
-//     const authenticated = await this.props.auth.isAuthenticated();
-//     if (authenticated !== this.state.authenticated) {
-//       this.setState({ authenticated });
-//     }
-//   }
-//
-//   componentDidUpdate() {
-//     this.checkAuthentication();
-//   }
-//
-//   render() {
-//     if (this.state.authenticated === null) return null;
-//     return this.state.authenticated ?
-//       <Redirect to={{ pathname: '/' }}/> :
-//       <LoginForm baseUrl={this.props.baseUrl} />;
-//   }
-// });
-
 import React, { Component } from 'react';
 import * as OktaSignIn from '@okta/okta-signin-widget';
 import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
