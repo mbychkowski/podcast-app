6b1e4c621061dd2d8158295fafe9fd8606047cfb
mjsimon91 <mjsimon91@gmail.com>
2018-05-05 18:01:05 -0400

    Podcasts are posting and the views are being updated

--- controllers/podcastShowController.js
+++ controllers/podcastShowController.js
@@ -19,9 +19,34 @@ module.exports = {
       .catch(err => res.json(dbModel))
   },
   addPodcast: function(req, res) {
-    db.PodcastShow
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(dbModel => res.json(dbModel))
+    //find if the entry already exists in the db
+    var query = db.PodcastShow.where({collectionId: req.body.collectionId})
+    query.findOne(function (err, results){
+      //If the query does not exist in the db
+      if (err) return handleError(err);
+      //if the query does exist in the
+      if (results) {
+        db.PodcastShow.findOneAndUpdate(
+        {collectionId: req.body.collectionId},
+        { $inc: {views: 1} },
+        {
+          upsert: true,
+          new: true
+        },
+        function(error, res){
+          if (error){
+            console.log(error)
+          } else {
+            console.log(res)
+          }
+        }) // Closes the findOneAndUpdate response
+      } else {
+        db.PodcastShow
+          .create(req.body)
+          .then(dbModel => res.json(dbModel))
+          .catch(dbModel => res.json(dbModel))
+      }
+    })
+
   }
 }
