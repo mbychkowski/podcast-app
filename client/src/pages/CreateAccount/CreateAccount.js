6ac9cb3b0db046ff485cad02b9a25fea9fc9ebe2
michael bychkowski <mbychkowski@gmail.com>
2018-05-04 22:19:49 -0400

    style form

--- client/src/pages/CreateAccount/CreateAccount.js
+++ client/src/pages/CreateAccount/CreateAccount.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import OktaAuth from '@okta/okta-auth-js';
 import { withAuth } from '@okta/okta-react';
 
+import "./CreateAccount.css"
+
 export default class SignupForm extends Component {
   constructor(props) {
     super(props);
@@ -31,29 +33,29 @@ export default class SignupForm extends Component {
 
   render() {
     return (
-      <div>
-        <div>
-          <h1>Sign Up</h1>
-          <form action="/" method="post">
-            <div className="flex flex-col mb-4">
-              <label className="mb-2 uppercase font-bold text-lg" for="first_name">First Name</label>
-              <input className="border py-2 px-3" type="text" name="first_name" id="first_name"/>
+      <div className="flex items-center h-screen w-full">
+        <div className="w-full bg-white shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
+          <h1 className="block w-full text-center mb-6">Sign Up</h1>
+          <form className="mb-4 md:flex md:flex-wrap md:justify-between" action="/" method="post">
+            <div className="field-group mb-4 md:w-1/2">
+              <label className="field-label" for="first_name">First Name</label>
+              <input className="field md:mr-2" type="text" name="first_name" id="first_name"/>
             </div>
-            <div className="flex flex-col mb-4">
-              <label className="mb-2 uppercase font-bold text-lg" for="last_name">Last Name</label>
-              <input className="border py-2 px-3" type="text" name="last_name" id="last_name"/>
+            <div className="field-group mb-4 md:w-1/2">
+              <label className="field-label md:ml-2" for="last_name">Last Name</label>
+              <input className="field md:ml-2" type="text" name="last_name" id="last_name"/>
             </div>
-            <div className="flex flex-col mb-4">
-              <label className="mb-2 uppercase font-bold text-lg" for="email">Email</label>
-              <input className="border py-2 px-3" type="email" name="email" id="email"/>
+            <div className="field-group mb-4 md:w-full">
+              <label className="field-label" for="email">Email</label>
+              <input className="field" type="email" name="email" id="email"/>
             </div>
-            <div className="flex flex-col mb-4">
-              <label className="mb-2 uppercase font-bold text-lg" for="password">Password</label>
-              <input className="border py-2 px-3" type="password" name="password" id="password"/>
+            <div className="field-group mb-4 md:w-full">
+              <label className="field-label" for="password">Password</label>
+              <input className="field" type="password" name="password" id="password"/>
             </div>
-            <button className="block bg-teal hover:bg-teal-darker text-white uppercase text-lg font-bold mx-auto p-4" type="submit">Create Account</button>
+            <button className="btn btn-teal mx-auto" type="submit">Create Account</button>
           </form>
-          <a className="block w-full text-center no-underline text-sm text-grey-dark hover:text-grey-darker" href="/login">Already have an Account?</a>
+          <a className="link link-grey w-full text-center" href="/login">Already have an Account?</a>
         </div>
       </div>
     )
