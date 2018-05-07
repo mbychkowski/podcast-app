916b771af73820c7b8e0ca045869a92f5bbb2218
Andrew Zoka <mugendiz@gmail.com>
2018-05-07 12:27:02 -0400

    made JumbotronEps responsive and changed styling

--- client/src/components/JumbotronEps/JumbotronEps.js
+++ client/src/components/JumbotronEps/JumbotronEps.js
@@ -4,28 +4,29 @@ import "./JumbotronEps.css"
 const JumbotronEps = props => {
   return (
 
-      <div className="flex-initial mx-auto jumbotron flex w-full mb-4">
+  // jumbotron UPGRADE
+
+  <div className="flex-initial mx-auto jumbotron flex w-full mb-4">
+
 {/* image */}
-        <div className="picInfo w-auto w-1/3">
-          <img src={props.image} alt="No Image"/>
-{/* author */}
-          <h3 className="author text-2xl leading-normal float-right">{props.author}</h3>
-        </div>
-{/* info */}
-        <div className="flex-grow text-lg w-1/3">
+    <img className="img float-left" src={props.image}></img>
+    <div className="main float-right shadow-md max-h-full rounded-b lg:rounded-b-none lg:rounded-r p-4 leading-normal">
+      <div className="mb-8 shadow-sm">
+
 {/* title */}
-          <h1 className=" podcastHeading float-none flex-auto leading-normal lg:leading-loose">{props.title}</h1>
+        <div className="podcastHeading float-left text-2xl">{props.title}</div>
+
 {/* description */}
-          <h3 className="description p-2 flex-w-full sm:overflow-y-scroll text-xlg">{props.description}</h3>
-          {/* md:invisible w-3/3 lg:visible xl:visible leading-normal */}
-
+        <div className="description max-h-screen flex-col h-32 p-2 text-base shadow-md overflow-y-scroll">
+          {props.description}
+          {/* author */}
+          <h4 className="author text-xl leading-normal float-right">{props.author}</h4>
         </div>
-
       </div>
-
-      )
-    }
-
+    </div>
+  </div>
+  )
+}
 
 //Export the Jumbotron
 export default JumbotronEps;
