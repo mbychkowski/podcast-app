aba2775ba5d85448b75a1b0eeb759f8be610e37d
mjsimon91 <mjsimon91@gmail.com>
2018-05-09 13:51:05 -0400

    Switching branches

--- models/index.js
+++ models/index.js
@@ -4,9 +4,6 @@ module.exports = {
   PodcastEpisode: require('./podcastEpisode'),
 =======
   Podcast: require('./podcastEpisode'),
-<<<<<<< HEAD
->>>>>>> parent of 88102838... Merge pull request #50 from mbychkowski/user-db
-=======
 >>>>>>> parent of 88102838... Merge pull request #50 from mbychkowski/user-db
   PodcastShow: require('./podcastShow')
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -5,10 +5,7 @@ const podcastShowRoutes = require("./podcastShow")
 const podcastEpisodeRoutes = require("./podcastEpisode")
 const userRoutes = require("./users");
 
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 88102838... Merge pull request #50 from mbychkowski/user-db
 // router.use("/user", userRoutes)
 router.use("/podcastShow", podcastShowRoutes)
 router.use("/podcastEpisode", podcastEpisodeRoutes)
