9abd1d7b746de5bff56deba1b4e594a483cf70e9
mjsimon91 <mjsimon91@gmail.com>
2018-05-09 13:35:32 -0400

    Reverting the merge

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
 module.exports = {
+<<<<<<< HEAD
 
   PodcastEpisode: require('./podcastEpisode'),
+=======
+  Podcast: require('./podcastEpisode'),
+>>>>>>> parent of 88102838... Merge pull request #50 from mbychkowski/user-db
   PodcastShow: require('./podcastShow')
 };
