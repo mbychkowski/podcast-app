611d843919fc790594d6c46bb400d1769c06271a
mjsimon91 <mjsimon91@gmail.com>
2018-05-09 13:58:59 -0400

    Trying to revert back to this commit

--- models/index.js
+++ models/index.js
@@ -1,9 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-
-  PodcastEpisode: require('./podcastEpisode'),
-=======
   Podcast: require('./podcastEpisode'),
->>>>>>> parent of 88102838... Merge pull request #50 from mbychkowski/user-db
-  PodcastShow: require('./podcastShow')
+  PodcastShow: require('./podcastShow'),
+  User: require('./user')
 };
