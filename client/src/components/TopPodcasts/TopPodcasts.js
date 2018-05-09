983bab5012580b0483ea70a69bce8768e08a9ae6
mjsimon91 <mjsimon91@gmail.com>
2018-05-09 12:42:33 -0400

    Revert "Content has been added to the home screen to show top podcast shows and episodes"
    This reverts commit 9e04f639f5746c38cbb87121d88e9dd7bf79a741.

--- client/src/components/TopPodcasts/TopPodcasts.js
+++ client/src/components/TopPodcasts/TopPodcasts.js
@@ -8,14 +8,14 @@ class TopPodcasts extends Component {
   render(){
     return(
       <div>
-        <h1 className="text-center">Top 10 Podcasts</h1>
+        <h1 className="mx-4">Top 10 Podcasts</h1>
 
-        <div className="w-5/6 ml-auto mr-auto flex group overflow-x-scroll ..." >
+        <div className="w-full flex flex-wrap group" >
 
 
           {/* Map the get call for the top ten podcasts to the component */}
           {this.props.searchTopTenShows.map(podcastShows => (
-            <div className="mx-4 my-2 border-r border-b border-l border-t border-grey-light w-full" key={podcastShows._id}>
+            <div className="group mx-4 my-2 border-r border-b border-l border-t border-grey-light flex-1" key={podcastShows._id}>
 
               {/* Provide the ability to pass information about the selected podcast to the displayed eposide list */}
               <Link to = {{
@@ -38,17 +38,17 @@ class TopPodcasts extends Component {
 
               // Remove the styling from the text for a URL
               style={{ textDecoration: 'none' }}>
-              <div className="w-full">
+              <div class="w-full lg:flex">
 
-                <div className="lg:border-l-0  lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 justify-between leading-normal">
+                <div className="lg:border-l-0  lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
 
                   <div className="mb-8">
 
-                    <div className="text-black font-bold text-l mb-2">{podcastShows.collectionName}</div>
+                    <div className="text-black font-bold text-xl mb-2">{podcastShows.collectionName}</div>
                   </div>
 
                   <div className="flex items-center">
-                    <img className="w-10 h-10 rounded-full mr-4" src={podcastShows.artworkUrl30} alt={podcastShows.collectionname}/>
+                    <img className="w-10 h-10 rounded-full mr-4" src={podcastShows.artworkUrl30} alt="Avatar of Jonathan Reinink"/>
                     <div className="text-sm">
                       <p className="text-black leading-none">{podcastShows.artistName}</p>
                       <p className="text-grey-dark">Views: {podcastShows.views}</p>
