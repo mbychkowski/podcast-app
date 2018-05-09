728e8c062ae12585678f6110f4f8252299a8ba04
Mugendi A. Zoka <32772345+mugendiz@users.noreply.github.com>
2018-05-09 14:33:03 -0400

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -37,7 +37,6 @@ export default withAuth(class Navbar extends Component {
         <Menu className="bg-black" fixed="top" inverted>
           <Container>
             <Menu.Item as="a" header href="/">
-              {/* <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> */}
               <span className="logo text-4xl font-semibold text-xl tracking-normal">Octar</span>
             </Menu.Item>
             {this.state.authenticated === true && <Menu.Item id="profile-button" as="a" href="/profile">Profile</Menu.Item>}
