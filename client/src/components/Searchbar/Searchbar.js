439230ee99c2ad393ea8986ceb8d550dd645d6ed
michael bychkowski <mbychkowski@gmail.com>
2018-05-09 18:38:11 -0400

    searc bar style finalized

--- client/src/components/Searchbar/Searchbar.js
+++ client/src/components/Searchbar/Searchbar.js
@@ -4,27 +4,29 @@ import "./Searchbar.css"
 const Searchbar = props =>  {
 
   return(
-    <div className="flex w-full items-center">
-      <form className="bg-transparent d:flex md:flex-wrap md:justify-between">
-        <div className="w-full max-w-lg">
-          <input
-            className="p-2 mr-2 search-field-style hover:border-pink"
-            type="text"
-            name="search"
-            placeholder="Find a podcast"
-            aria-label="Full name"
-            onChange={props.handleInputChange}
-            value = {props.search}
-          />
-          <button
-            className="p-3 search-button-style hover:bg-pink-darker"
-            type="button"
-            onClick = {props.handlePodcastSearch}
-          >
-            Search
-          </button>
-        </div>
-      </form>
+    <div className="flex items-center w-full">
+      <div className="w-full md:max-w-lg md:mx-auto">
+        <form>
+          <div className="flex flex-row">
+            <input
+              className="flex-1 p-2 mr-2 search-field-style hover:border-pink"
+              type="text"
+              name="search"
+              placeholder="Find a podcast"
+              aria-label="Full name"
+              onChange={props.handleInputChange}
+              value = {props.search}
+              />
+            <button
+              className="p-3 search-button-style hover:bg-pink-darker"
+              type="button"
+              onClick = {props.handlePodcastSearch}
+              >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
