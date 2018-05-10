061d9c8d9a4e43c0b68ca03fd0cb1cbe3a583982
mjsimon91 <mjsimon91@gmail.com>
2018-05-10 18:56:02 -0400

    Merge conflicts fixed

--- client/src/components/Searchbar/Searchbar.js
+++ client/src/components/Searchbar/Searchbar.js
@@ -65,7 +65,7 @@ class Searchbar extends Component {
                 <button
                   className="flex-no-shrink bg-teal hover:bg-teal-dark border-teal hover:border-teal-dark text-sm border-4 text-white py-1 px-2 rounded"
                   type="button"
-                  onSubmit = {this.returnKeyPress}
+                  // onSubmit = {this.returnKeyPress}
                   // onKeyPress = {}
                   >
                 Search
