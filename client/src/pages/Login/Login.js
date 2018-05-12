1693d460296b932a2f67cbc6029331a071a6e53e
michael bychkowski <mbychkowski@gmail.com>
2018-05-12 07:24:45 -0400

    change login configuration

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -14,7 +14,7 @@ export default class Login extends Component {
        */
       baseUrl: process.env.REACT_APP_ISSUER.split('/oauth2')[0],
       clientId: process.env.REACT_APP_CLIENT_ID,
-      redirectUri: process.env.REACT_APP_REDIRECT_URI,
+      redirectUri: "{{redirectUri configured in OIDC app}}",
       logo: '/logo.svg',
       i18n: {
         en: {
